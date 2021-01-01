e5a121f4085c6b4babe00a2a7f733922e4997762
Axel Ancona Esselmann <axel@anconaesselmann.com>
2021-01-01 01:34:47 -0800

    Bugfix

--- SwiftGpx.podspec
+++ SwiftGpx.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'SwiftGpx'
-  s.version          = '0.1.1'
+  s.version          = '0.1.2'
   s.summary          = 'SwiftGpx is a small library for reading amd writing GPX files.'
 
 # This description is used to generate tags and improve search results.
--- SwiftGpx/Classes/SwiftGpx.swift
+++ SwiftGpx/Classes/SwiftGpx.swift
@@ -83,15 +83,27 @@ public struct LocationTrack {
         self.timestamp = timestamp
         self.trackSegments = trackSegments
     }
+
+    public init(name: String, trackSegments: [LocationTrackSegment] = []) {
+        self.name = name
+        self.timestamp = trackSegments.first?.locations.first?.timestamp ?? Date()
+        self.trackSegments = trackSegments
+    }
     
     public init?(gpxJson: [String: Any]) {
         guard
             let trksegElements = gpxJson[Keys.trksegElements] as? [[String: Any]],
-            let name = gpxJson[Keys.name] as? String,
+            let name = gpxJson[Keys.name] as? String
+        else { return nil }
+        if
             let timeString = gpxJson[Keys.time] as? String,
             let timestamp = Date(fromString: timeString, format: .isoDateTimeSec, timeZone: .utc)
-        else { return nil }
-        self.init(name: name, timestamp: timestamp, trackSegments: trksegElements.compactMap(LocationTrackSegment.init(gpxJson:)))
+        {
+            self.init(name: name, timestamp: timestamp, trackSegments: trksegElements.compactMap(LocationTrackSegment.init(gpxJson:)))
+        } else {
+            self.init(name: name, trackSegments: trksegElements.compactMap(LocationTrackSegment.init(gpxJson:)))
+        }
+
     }
 }
 
